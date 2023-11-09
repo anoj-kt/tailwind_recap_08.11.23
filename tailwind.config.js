cdc99b2884c48b61db9f5c8d24c4f579acfb1211
anoj-kt <anojk28@gmail.com>
2023-11-09 09:04:07 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8db38b',
+        secondary: '#56876d',
+      },
+      fontFamily: {
+        cursive: 'Edu TAS Beginner, cursive',
+      },
+    },
+  },
+  plugins: [],
+};
